1799f03e5d05f4b6538843c7b530b01dc86349c4
Manisai Perumandla <manisaims22@gmail.com>
2026-09-03 10:59:47 +0530

    Time: 631 ms (40.2%), Space: 0B (100%) - LeetHub

---
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT date_id,make_name,
+COUNT(distinct lead_id) as unique_leads,COUNT(distinct partner_id) as unique_partners
+from DailySales
+group by date_id,make_name;
